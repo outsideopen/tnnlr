d7179fbaaea2e62e5f07a7f254c2398a5b6f85b8
Alexander Standke <xanderstrike@gmail.com>
2014-09-17 15:19:31 -0700

    add inside and outside ip reporting

--- server/app/controllers/client_controller.rb
+++ server/app/controllers/client_controller.rb
@@ -10,6 +10,8 @@ class ClientController < ActionController::Base
     client = Client.find_or_create_by(hostname: params[:hostname])
     client.assign_port
     client.last_report = Time.now
+    client.local_ip = params[:local_ip]
+    client.outside_ip = params[:outside_ip]
 
     if client.save
       render text: client.assign_port
